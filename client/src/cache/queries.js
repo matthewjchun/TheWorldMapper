c73c2e5c4bf303634a09f4821bdc3b2c92b729cb
matthewjchun <matthewchun.18@gmail.com>
2021-05-03 14:09:15 -0400

    initial commit

---
+++ client/src/cache/queries.js
@@ -0,0 +1,31 @@
+import { gql } from "@apollo/client";
+
+export const GET_DB_USER = gql`
+	query GetDBUser {
+		getCurrentUser {
+			_id
+			firstName
+			lastName
+			email
+		}
+	}
+`;
+
+export const GET_DB_TODOS = gql`
+	query GetDBTodos {
+		getAllTodos {
+			_id
+			name
+			owner
+			items {
+				_id
+				description
+				due_date
+				assigned_to
+				completed
+			}
+			sortRule
+			sortDirection
+		}
+	}
+`;
